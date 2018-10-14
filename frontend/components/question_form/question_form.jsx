01ca8752a17fba0e3fdd3fce1985cddc93b25d12
Luke Dreyer <lukepdreyer@gmail.com>
2018-10-14 12:48:35 -0700

    rendered question show below question edit form with manila background

--- frontend/components/question_form/question_form.jsx
+++ frontend/components/question_form/question_form.jsx
@@ -24,81 +24,97 @@ class QuestionForm extends React.Component {
     if(nextProps.question){
       this.setState({ ['title']: nextProps.question.title,
         ['body']:nextProps.question.body})
+      }
     }
-  }
 
-  navigateToSearch() {
-    this.props.history.push('/');
-  }
+    navigateToSearch() {
+      this.props.history.push('/');
+    }
 
-  update(property){
-    return e => this.setState({
-      [property]: e.target.value
-    });
-  }
-  askAQuestion(component){
-    return (
-      <div className="content">
-        <AsideNav links={1}/>
-      <div className="single-question-show">
-        <div className="header-section">
-          <div className="question-index-header">
-            <h1 className="question-title">{this.props.formType} A Question</h1>
+    update(property){
+      return e => this.setState({
+        [property]: e.target.value
+      });
+    }
+
+    editShow(){
+      if(this.props.formType === 'Edit'){
+        return(
+          <div >
+            <div className="header-section manila">
+              <h1 className="question-title">{this.state.title}</h1>
+            </div>
+            <div className="question-body manila">
+              {this.state.body}
+            </div>
+          </div>
+        );
+      } else return (<div></div>);
+    }
+    askAQuestion(component){
+      return (
+        <div className="content">
+          <AsideNav links={1}/>
+          <div className="single-question-show">
+            <div className="header-section">
+              <div className="question-index-header">
+                <h1 className="question-title">{this.props.formType} A Question</h1>
+              </div>
+            </div>
+            <div className="question-body">
+              {component}
+            </div>
+            {this.editShow()}
           </div>
         </div>
-        <div className="question-body">
-          {component}
-        </div>
-      </div>
-    </div>
-    );
-  }
+      );
+    }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append('question[categoryId]', this.state.categoryId);
-    formData.append('question[userId]', this.state.userId);
-    formData.append('question[title]', this.state.title);
-    formData.append('question[body]', this.state.body);
-    this.props.questionAction(formData);
-    this.navigateToSearch();
-  }
-  render(){
-    const { title, body } = this.state;
-    return(
-      <div className="new-question-container">
-        {this.askAQuestion(
-          <form onSubmit={this.handleSubmit}>
-            <label className="question-field">
-              Title
-              <br />
-              <input
-                type="text"
-                value={title}
-                onChange={this.update('title')}
-                className="question-input-field"
-                />
-            </label>
-            <br />
-            <label className="question-field">
-              Body
-              <br />
-              <div className="question-text-input-area">
-                <textarea
-                  value={body}
-                  onChange={this.update('body')}
+    handleSubmit(e) {
+      e.preventDefault();
+      const formData = new FormData();
+      formData.append('question[categoryId]', this.state.categoryId);
+      formData.append('question[userId]', this.state.userId);
+      formData.append('question[title]', this.state.title);
+      formData.append('question[body]', this.state.body);
+      this.props.questionAction(formData);
+      this.navigateToSearch();
+    }
+    render(){
+      const { title, body } = this.state;
+      return(
+        <div className="new-question-container">
+          {this.askAQuestion(
+            <form onSubmit={this.handleSubmit}>
+              <label className="question-field">
+                Title
+                <br />
+                <input
+                  type="text"
+                  value={title}
+                  onChange={this.update('title')}
                   className="question-input-field"
                   />
-              </div>
-            </label>
-            <br />
-            <input type="submit" value={`${this.props.formType} Question`}
-              className="new-question-button" />
-          </form>
-        )}
-      </div>
-    );
+              </label>
+              <br />
+              <label className="question-field">
+                Body
+                <br />
+                <div className="question-text-input-area">
+                  <textarea
+                    value={body}
+                    onChange={this.update('body')}
+                    className="question-input-field"
+                    />
+                </div>
+              </label>
+              <br />
+              <input type="submit" value={`${this.props.formType} Question`}
+                className="new-question-button" />
+            </form>
+          )}
+        </div>
+      );
+    }
   }
-}
-export default withRouter(QuestionForm);
+  export default withRouter(QuestionForm);
