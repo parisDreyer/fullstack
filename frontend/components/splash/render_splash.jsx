faec515e51a1387435790a591514b26560da4e57
Luke Dreyer <lukepdreyer@gmail.com>
2018-10-15 11:33:50 -0700

    set images to window to format them for heroku

--- frontend/components/splash/render_splash.jsx
+++ frontend/components/splash/render_splash.jsx
@@ -7,8 +7,14 @@ import SplashSignupContainer from './splash_container';
 const RenderSplash = ({user}) => {
   if(!user) //&& window.location.toString().match(/\/#\/.*$/)[0] === "/#/")
   {
+    const bkgrnd = {background: `linear-gradient(
+        rgba(0,255,0,0.45),
+        rgba(0,0,255,0.45)
+        ),url(${window.images.heroBanner})`,
+        backgroundSize: 'cover',
+      };
     return (
-      <div className="old-hero">
+      <div className="old-hero" style={bkgrnd}>
         <div className="splash-explanation-text">
           <h3>Learn, Share, Build</h3>
           <p>
