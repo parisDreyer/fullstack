86bd31eaecd4022fa5a4ae9a5813833ba5311903
Luke Dreyer <lukepdreyer@gmail.com>
2018-10-15 17:57:55 -0700

    styled sidebar with links

--- frontend/components/nav/aside_nav.jsx
+++ frontend/components/nav/aside_nav.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 const AsideNav = navLinks => (
   <div className="left-sidebar">
     <div className="left-sidebar-sticky-container">
+      <div className="space-li-header">Public</div>
       <ul>
-        <li>Sample Link 1</li>
-        <li>Sample Link 2</li>
-        <li>Sample Link 3</li>
+        <li className="space-li">
+          <Link to="/questions" className="grey-beige">
+            <strong>HOME</strong>
+          </Link>
+        </li>
+        <li className="space-li">
+          <Link to="/questions" className="grey-beige">
+            <div className="logo-container">
+              <div className="logo">
+                <img src={window.images.logo}
+                  alt="site header logo is a drawing of the rings of hell
+                  as described in Dante Alighieri's epic poem Inferno" />
+              </div>
+              stack <strong>inferno</strong>
+            </div>
+          </Link>
+        </li>
+        <li className="space-li">
+          <Link to="/questions" className="grey-beige">
+            <strong>Jobs</strong>
+          </Link>
+        </li>
       </ul>
     </div>
   </div>
