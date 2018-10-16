817e8fb114bd1408e5837034ea089475d5d6ce84
Luke Dreyer <lukepdreyer@gmail.com>
2018-10-16 15:01:52 -0700

    made a working search bar. now just need to refine it's functionality between states/routes

--- frontend/components/search/search_show.jsx
+++ frontend/components/search/search_show.jsx
@@ -13,11 +13,34 @@ class SearchShow extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = { text: $('input.nav-search-bar').val() };
+    this.state = {
+      limit: this.props.limit,
+      offset: this.props.offset,
+      text: $('input.nav-search-bar').val()
+    };
+
+    this.handleSearchClick = this.handleSearchClick.bind(this);
+
   }
 
   componentDidMount(){
-    this.props.updateFilter({limit: this.props.limit});
+    this.props.updateFilter(
+      'text', this.state.text
+      // limit: this.props.limit,
+      // text: this.state.text,
+      // offset: this.props.offset,
+    );
+  }
+
+  handleSearchClick(e){
+    e.preventDefault();
+    this.setState({
+      text: e.target.children[0].value
+    });
+    this.props.updateFilter('text', e.target.children[0].value
+      // limit: this.state.limit,
+      // offset: this.state.offset
+    );
   }
 
   render(){
@@ -36,7 +59,12 @@ class SearchShow extends React.Component {
                   limit={this.props.limit}
                   updateFilter={this.props.updateFilter}
                   />
+                <form onSubmit={this.handleSearchClick} className="nav-search-bar-container">
+                  <input className="nav-search-bar" type="text" />
+                  <input type="submit" className="search-icon" value="> "/>
+                </form>
               </div>
+
             </div>
             <QuestionIndex questions={this.props.questions} />
           </div>
