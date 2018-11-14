a6b4de0984bba8c9eb42d06fcfb50c7f105361f2
Luke Dreyer <lukepdreyer@gmail.com>
2018-11-14 09:08:47 -0800

    made header nav inputs a uniform size

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 const Greeting = ({currentUser, logout, demoLogin}) => {
   const sessionLinks = () => (
     <li className="login-signup">
-      <button className="signup" onClick={demoLogin}>Demo Login</button>
+      <button className="demo-login" onClick={demoLogin}>Demo Login</button>
       <Link to="/login" className="login">Login</Link>
       <Link to="/signup" className="signup">Sign up!</Link>
     </li>
