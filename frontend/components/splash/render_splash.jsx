3b7af0e7c3459b76e3e2b27de0a3de34e913292e
Luke Dreyer <lukepdreyer@gmail.com>
2018-11-14 10:23:34 -0800

    rendered more functional image on frontend

--- frontend/components/splash/render_splash.jsx
+++ frontend/components/splash/render_splash.jsx
@@ -7,30 +7,26 @@ import SplashSignupContainer from './splash_container';
 const RenderSplash = ({user}) => {
   if(!user) //&& window.location.toString().match(/\/#\/.*$/)[0] === "/#/")
   {
-    const bkgrnd = {background: `linear-gradient(
+    const bkgrnd = { background: `linear-gradient(
         rgba(0,255,0,0.45),
         rgba(0,0,255,0.45)
-        ),url(${window.images.heroBanner})`,
-        backgroundSize: '100% 100%',
-        backgroundRepeat: 'no-repeat'
-      };
-    return (
-      <>
-      <div style={{height: "42px"}}></div>
-      <div className="old-hero" style={bkgrnd}>
-        <div className="splash-explanation-text">
-          <h3>Learn, Share, Build</h3>
-          <p>
-            Each month, over 50 million denizens come to Stack Inferno to learn, share
-            their knowledge, and maunder through eternity. Join the world’s largest denizen
-            community.
-          </p>
+        ),url(${window.images.fadedWriting})`, backgroundSize: "100% 100%", backgroundRepeat: "no-repeat" };
+    // http://skybots.net/scrap/InvertRGB // <-Credits for splash image
+    return <>
+        <div style={{ height: "42px" }} />
+        <div className="old-hero" style={bkgrnd}>
+          <div className="splash-explanation-text">
+            <h3>Learn, Share, Build</h3>
+            <p>
+              Each month, over 50 million denizens come to Stack Inferno to
+              learn, share their knowledge, and maunder through eternity. Join
+              the world’s largest denizen community.
+            </p>
+          </div>
+          <div className="old-hero-background" />
+          <SplashSignupContainer />
         </div>
-        <div className="old-hero-background"></div>
-        <SplashSignupContainer />
-      </div>
-      </>
-    );
+      </>;
   } else return (<div></div>);
 }
 
